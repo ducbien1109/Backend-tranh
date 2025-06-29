8869f92bbfe76a7efffe19eaa81c84676fcce73d
ducbien1109 <ducbien911@gmail.com>
2025-06-29 09:54:10 +0700

    cap nhat moi nhat 1

--- server.js
+++ server.js
@@ -1,76 +1,5 @@
-// // backend/server.js
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const Product = require("./product.model");
-// const Order = require("./orders.model");
-// // const User = require("../models/User");
-// const app = express();
-// app.use(cors());
-// // app.use(express.json());
-// app.use(express.json({ limit: "10mb" }));
 
-// mongoose
-//   .connect("mongodb://localhost:27017/productdb")
-//   .then(() => console.log("✅ Connected to MongoDB"))
-//   .catch((err) => console.error("❌ MongoDB connection error:", err));
-
-// // API routes
-// app.get("/products", async (req, res) => {
-//   const items = await Product.find();
-//   res.json(items);
-// });
-// // Thêm vào server.js
-// app.get("/products/:id", async (req, res) => {
-//   try {
-//     const product = await Product.findById(req.params.id);
-//     res.json(product);
-//   } catch (error) {
-//     res.status(404).json({ message: "Không tìm thấy sản phẩm" });
-//   }
-// });
-
-// app.post("/products", async (req, res) => {
-//   const newItem = new Product(req.body); // ✅ dùng req.body để nhận đầy đủ dữ liệu
-//   await newItem.save();
-//   res.json(newItem);
-// });
-
-// app.put("/products/:id", async (req, res) => {
-//   const updated = await Product.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//   });
-//   res.json(updated);
-// });
-
-// app.delete("/products/:id", async (req, res) => {
-//   await Product.findByIdAndDelete(req.params.id);
-//   res.json({ message: "Deleted" });
-// });
-// //order
-// app.post("/orders", async (req, res) => {
-//   try {
-//     const newOrder = new Order(req.body);
-//     await newOrder.save();
-//     res.json(newOrder);
-//   } catch (error) {
-//     console.error("Lỗi khi tạo đơn hàng:", error);
-//     res.status(500).json({ message: "Không thể tạo đơn hàng" });
-//   }
-// });
-
-// // API lấy danh sách đơn hàng
-// app.get("/orders", async (req, res) => {
-//   try {
-//     const orders = await Order.find().populate("productId");
-//     res.json(orders);
-//   } catch (error) {
-//     res.status(500).json({ message: "Không thể lấy đơn hàng" });
-//   }
-// });
-// app.listen(5000, () => console.log("Server running on http://localhost:5000"));
-// //
-
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -86,11 +15,17 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 // Kết nối MongoDB
+// mongoose
+//   .connect("mongodb://localhost:27017/productdb", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
 mongoose
-  .connect("mongodb://localhost:27017/productdb", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
+
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
@@ -215,7 +150,7 @@ app.get("/users", async (req, res) => {
 
 // ------------------------------------------------- //
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
